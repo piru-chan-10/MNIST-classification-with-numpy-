3ac810e7f0303a4bf50721011f05db6f71fbfdd9
piru-chan-10 <priyanshu792005@gmail.com>
2026-05-07 16:57:45 +0530

    Delete TCS directory

--- TCS/3ball.py
+++
@@ -1,5 +0,0 @@
-''' the problem is to find the number of combinations of placing three balls such that two consecutive balls are not of same color
-    we gonna use DP for it 
-'''
-def solution(G,Y,R):
-  pass 
